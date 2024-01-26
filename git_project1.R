af88bdd91ba3716492f2d463b97d656dc7f62fbc
Gillian Fanning <fanningge@vcu.edu>
2024-01-26 11:29:56 -0500

    first version of project 1

---
+++ git_project1.R
@@ -0,0 +1,15 @@
+#project 1
+#gillian fanning
+#------------------------------------------------------------------------
+
+#load libraries
+library(ggplot2)
+
+#load data
+mtcars
+
+#create plot
+ggplot(mtcars, aes(x = disp, y = mpg, color = factor(cyl))) +
+  geom_point() +
+  labs(title = "Scatterplot of MPG by Displacement", x = "Displacement", y = "MPG") +
+  theme_minimal()
